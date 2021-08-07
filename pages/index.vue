f313ad63e20dda3a17e07e9facb5c5af9a2dd862
Alex <alex_billson@outlook.com>
2021-08-07 11:22:45 +1000

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1>Hi, I'm Alex.</h1>
+    <div class="row">
+      <div class="one-half column">
+        <p>I'm a Software Developer based in Melbourne, Australia.</p>
+        <p>
+          Welcome to my website, it's not a lot but you can learn more about me
+          by:
+        </p>
+        <ul>
+          <li>Connecting on <a target="_blank" href="https://www.linkedin.com/in/alex-billson-69a0ba13b">LinkedIn</a></li>
+          <li>
+            Browsing my code on
+            <a target="_blank" href="https://github.com/pielegacy">GitHub</a>
+          </li>
+          <li>
+            Contacting me via
+            <a href="mailto:alex_billson@outlook.com">Email</a>
+          </li>
+        </ul>
+      </div>
+      <div class="one-half column">
+        <img class="u-max-full-width" src="~/assets/me.jpg" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
